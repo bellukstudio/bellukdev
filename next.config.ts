b37f1f3a2e9d1c9ddb0fac30f3f9d46def4a00ef
bellukchips <51257520+Bellukchips@users.noreply.github.com>
2026-08-05 16:46:01 +0700

    Add Cloudinary image optimization configuration

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,11 @@ const nextConfig: NextConfig = {
             },
           ]
         : []),
+       {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/ducsvvqsy/**',
+      },
     ],
   },
 };
